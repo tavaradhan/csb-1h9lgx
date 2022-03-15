140716d0d7553a09f8db9d315f453f017c2a57ee
Code Sandbox <hello@codesandbox.io>
2022-03-15 05:38:32 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.0fcdbfe1e6b9610a8b3204a2e8a05889.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "298bfe086bdfa7af31032d169065b47e",
+    "url": "https://tavaradhan.github.io/csb-1h9lgx/index.html"
+  },
+  {
+    "revision": "b8d339e70dee254c5bff",
+    "url": "https://tavaradhan.github.io/csb-1h9lgx/static/css/2.a2739337.chunk.css"
+  },
+  {
+    "revision": "b8d339e70dee254c5bff",
+    "url": "https://tavaradhan.github.io/csb-1h9lgx/static/js/2.aadcd3ad.chunk.js"
+  },
+  {
+    "revision": "aeeac8a55c40abbe73fc",
+    "url": "https://tavaradhan.github.io/csb-1h9lgx/static/js/main.77937777.chunk.js"
+  },
+  {
+    "revision": "c20ca075fea880cf9d15",
+    "url": "https://tavaradhan.github.io/csb-1h9lgx/static/js/runtime~main.f048f540.js"
+  }
+]);
